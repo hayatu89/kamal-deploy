62bf9254dabca77969aaf7609dc56f492f2c59d0
Hayatu Abubakar <hayatu89@live.com>
2023-10-04 08:49:49 +0100

    minor changes to deploy token;

--- config/runtime.exs
+++ config/runtime.exs
@@ -51,20 +51,21 @@ if config_env() == :prod do
       environment variable SECRET_KEY_BASE is missing.
       You can generate one by calling: mix phx.gen.secret
       """
-
+  host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "3000")
 
   config :kamal, KamalWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "3000")],
+  http: [:inet6, port: port],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port:  String.to_integer(System.get_env("PORT") || "3000")
+      port:  port
     ],
-    secret_key_base: secret_key_base,
-    check_origin: ["http://159.65.88.129"]
+    check_origin: ["http://159.65.88.129"],
+    secret_key_base: secret_key_base
 
   # ## SSL Support
   #
